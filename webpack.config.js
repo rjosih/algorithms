90e420a7fceea4a14d47108a5d417cfe80e3049b
Rosa Ekström <rosa.ekstrom@regionhalland.se>
2023-01-14 20:44:25 +0100

    Try other changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.tsx",
-  // mode: "production",
+  mode: "production",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   devServer: {
     historyApiFallback: true,
   },
